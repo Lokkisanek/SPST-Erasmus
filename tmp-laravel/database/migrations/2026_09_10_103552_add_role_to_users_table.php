1f385f202776a57c091f6de5dae3f1ca4fd235d4
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 08:33:03 +0200

    Pridani potřebnych tables

---
+++ tmp-laravel/database/migrations/2026_09_10_103552_add_role_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->defualt('zak'); //sloupec role s defualtem zak -> ucitel, koordinator, admin, superadmin
+            $table->boolean('role')->default(true); //sloupec jestli je user aktivni
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role', 'is_active']);
+        });
+    }
+};
