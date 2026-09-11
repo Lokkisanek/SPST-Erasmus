5ecd7a3664bc7a6f05d52d2f9e1b296d88d26b24
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 12:52:28 +0200

    vztah mezi tabulkami Student.php

--- tmp-laravel/app/Models/Student.php
+++ tmp-laravel/app/Models/Student.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function schoolClass() {
+        return $this->belongsTo(SchoolClass::class, 'class_id');
+    }
+
+    public function applications() {
+        return $this->hasMany(Application::class);
+    }
 }
