b24b10ae1573b51be4c83a60d5b6585796582650
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 13:06:00 +0200

    Vztah mezi tabulkami Application.php

--- tmp-laravel/app/Models/Application.php
+++ tmp-laravel/app/Models/Application.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    //
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function mobility()
+    {
+        return $this->belongsTo(Mobility::class);
+    }
+
+    public function classTeacher()
+    {
+        return $this->belongsTo(Teacher::class, 'class_teacher_id');
+    }
+
+    public function englishTeacher()
+    {
+        return $this->belongsTo(Teacher::class, 'english_teacher_id');
+    }
 }
