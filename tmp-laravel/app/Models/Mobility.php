023714140243a90aa1eb4781703ab9a6363e16e9
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 13:05:29 +0200

    Vztah mezi tabulkami Mobility.php

--- tmp-laravel/app/Models/Mobility.php
+++ tmp-laravel/app/Models/Mobility.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mobility extends Model
 {
-    //
+    public function schoolClass()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
