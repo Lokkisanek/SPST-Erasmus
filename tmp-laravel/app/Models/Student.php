14c2355fab1af7eecb4b09913d03b10f36afdd59
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 13:08:40 +0200

    Oprava překlepu

--- tmp-laravel/app/Models/Student.php
+++ tmp-laravel/app/Models/Student.php
@@ -11,7 +11,7 @@ class Student extends Model
     }
 
     public function schoolClass() {
-        return $this->belongsTo(SchoolClass::class, 'class_id');
+        return $this->belongsTo(Classes::class, 'class_id');
     }
 
     public function applications() {
