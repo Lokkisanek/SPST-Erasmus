8d8e8039a418a47f686de9d692030eaa30293e51
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 13:05:02 +0200

    Vztah mezi tabulkami Classes.php

--- tmp-laravel/app/Models/Classes.php
+++ tmp-laravel/app/Models/Classes.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Classes extends Model
 {
-    //
+    public function students()
+    {
+        return $this->hasMany(Student::class, 'class_id');
+    }
+
+    public function mobilities()
+    {
+        return $this->hasMany(Mobility::class, 'class_id');
+    }
 }
