9286b0371c84e6e14fd6a40f6af8deb34a1c4373
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 13:15:42 +0200

    test dashboard pro přihlašovani pod jinymi rolemi

--- tmp-laravel/resources/views/dashboard.blade.php
+++ tmp-laravel/resources/views/dashboard.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @if (auth()->user()->role === 'zak')
+                    <p>žák</p>
+                    @elseif (auth()->user()->role === 'ucitel')
+                    <p>učitel</p>
+                    @else
+                    <p>koordinator</p>
+                    @endif
                 </div>
             </div>
         </div>
