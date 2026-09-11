0421489abcd9c6a811334e783d52fa99dc06557b
Matyaš Odehnal <matyod@seznam.cz>
2026-09-11 13:04:34 +0200

    pridani dalsich vztahu do Teacher.php

--- tmp-laravel/app/Models/Teacher.php
+++ tmp-laravel/app/Models/Teacher.php
@@ -6,8 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
+    public function classTeacherApplications()
+    {
+        return $this->hasMany(Application::class, 'class_teacher_id');
+    }
+
+    public function englishTeacherApplications()
+    {
+        return $this->hasMany(Application::class, 'english_teacher_id');
+    }
 }
